663f412bb1c35214c53e567c4f3fa53aa18304e1
liusiyang <xjtu_liusiyang@163.com>
2021-04-21 23:44:16 +0800

    add the program that prints hello world

---
+++ TheCProgrammingLanguage/Code/helloworld/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // header file
+
+int main() // function main, the entry of this program
+{          // the body of the main function, it should be enclosed between braces
+	printf("hello world\n");
+	return 0;   // return a value to the caller
+}
